4af7ddbb4ac46437cfdf41808a1b57321fc714d6
Sarah Estrellado <sarah.estrellado@icloud.com>
2019-05-25 17:59:07 -0500

    added backgroundContext in DataController

--- Mooskine/Model/DataController.swift
+++ Mooskine/Model/DataController.swift
@@ -12,6 +12,7 @@ import CoreData
 class DataController {
     
     let persistentContainer: NSPersistentContainer
+    var backgroundContext: NSManagedObjectContext!
     
     var viewContext: NSManagedObjectContext {
         return persistentContainer.viewContext
@@ -21,12 +22,21 @@ class DataController {
         persistentContainer = NSPersistentContainer(name: modelName)
     }
     
+    func configureContexts() {
+        backgroundContext = persistentContainer.newBackgroundContext()
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+    }
+    
     func load(completion: (() -> Void)? = nil) {
         persistentContainer.loadPersistentStores { (storeDescription, error) in
             guard error == nil else {
                 fatalError(error!.localizedDescription)
             }
             self.autoSaveViewContext()
+            self.configureContexts()
             completion?()
         }
         
